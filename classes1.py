67306a8fa1945649977f95ed5b9409d3ca2124a0
debskiszymon <debski.szymon@gmail.com>
2019-10-16 16:24:46 +0200

    first commit

---
+++ classes1.py
@@ -0,0 +1,6 @@
+class Country():
+    def __init__(self, hotel, food, transportation, fun):
+        self.hotel= hotel
+        self.food = food
+        self.transportation = transportation
+        self.fun = fun
